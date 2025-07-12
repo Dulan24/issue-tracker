af5b7b0317219b8fa4542dfdcda9b9281e6aeae5
Dulan24 <Dulanlokugeegana@gmail.com>
2025-07-12 18:55:11 +0530

    handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,53 @@
 'use client'
 
-import { Button, TextArea, TextField, ThemePanel } from '@radix-ui/themes'
+import { Button, Callout, TextArea, TextField, ThemePanel } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, useController, Controller } from 'react-hook-form';
+import axios from 'axios';
 import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation';
+
+interface IssueForm{
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const {register, control, handleSubmit, formState:{errors}} = useForm<IssueForm>();
+  
+
   return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root placeholder="Title">
-        </TextField.Root>
-        <SimpleMDE placeholder='Description'>
-        </SimpleMDE>
+    <form 
+      className='max-w-xl space-y-3' 
+      onSubmit={handleSubmit(async(data) => {
+        console.log(data);
+        await axios.post('/api/issues', data);
+        router.push('/issues')
+      })}
+    >
+        <TextField.Root 
+          placeholder="Title" 
+          {...register('title', {required: true})} 
+        />
+        {errors.title &&(
+          <Callout.Root color='red'>
+            <Callout.Text>Title is required.</Callout.Text>
+          </Callout.Root>
+        )}
+        <Controller 
+          name='description'
+          control={control}
+          rules={{ required: true }}
+          render={({field})=><SimpleMDE placeholder='Description' {...field}/>}
+        />
+        {errors.description && (
+          <Callout.Root color='red'>
+            <Callout.Text>The description is required.</Callout.Text>
+          </Callout.Root>
+        )}
         <Button>Submit New Issue</Button>
-    </div>
+    </form>
   )
 }
 
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,6 +3,9 @@ import { Button } from '@radix-ui/themes'
 import Link from 'next/link'
 
 const IssuesPage = () => {
+  
+  console.log("Went to the issue page")
+  
   return (
     <div>
       <Button size="3" variant="soft">
